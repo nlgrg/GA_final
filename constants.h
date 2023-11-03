50438475fba07a1e1913ba3bfebd9e0549133d89
nlgrg <132469812+nlgrg@users.noreply.github.com>
2023-11-03 20:02:32 +0530

    Initial commit

---
+++ constants.h
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define STAR_MIN_PIXEL 3
+#define STAR_MAX_PIXEL 150
+#define MAX_STARS 100
+#define SKIP_PIXELS 2
+#define LENGTH 1821
+#define BREADTH 1821
+#define PIXEL_WIDTH 0.00000155
+#define NUM_MAX_STARS 13
+//SM constants
+#define FOCAL_LENGTH 0.0175
+#define EPSILON 2.22e-15
+#define DELTA 4.9417133613238384e-05
+#define ANG_DIST_TOLERANCE 1.2
+#define N_GC 8876
+#define N_KVEC_PAIRS 625957
+#define Y_MAX 0.999999999992621
+#define Y_MIN 0.983276956198341
+#define TOL 0.41224489795918373
+#define P1 37.244897959183675
+#define P2 85.51020408163265 
+#define EPSILON_SEQ_ERROR 0.005346938775510204 
+#define EPSILON_EST 0.001 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define STAR_MIN_PIXEL 3
+#define STAR_MAX_PIXEL 150
+#define MAX_STARS 100
+#define SKIP_PIXELS 2
+#define LENGTH 1821
+#define BREADTH 1821
+#define PIXEL_WIDTH 0.00000155
+#define NUM_MAX_STARS 13
+//SM constants
+#define FOCAL_LENGTH 0.0175
+#define EPSILON 2.22e-15
+#define DELTA 4.9417133613238384e-05
+#define ANG_DIST_TOLERANCE 1.2
+#define N_GC 8876
+#define N_KVEC_PAIRS 625957
+#define Y_MAX 0.999999999992621
+#define Y_MIN 0.983276956198341
+#define TOL 0.41224489795918373
+#define P1 37.244897959183675
+#define P2 85.51020408163265 
+#define EPSILON_SEQ_ERROR 0.005346938775510204 
+#define EPSILON_EST 0.001 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define STAR_MIN_PIXEL 3
+#define STAR_MAX_PIXEL 150
+#define MAX_STARS 100
+#define SKIP_PIXELS 2
+#define LENGTH 1821
+#define BREADTH 1821
+#define PIXEL_WIDTH 0.00000155
+#define NUM_MAX_STARS 13
+//SM constants
+#define FOCAL_LENGTH 0.0175
+#define EPSILON 2.22e-15
+#define DELTA 4.9417133613238384e-05
+#define ANG_DIST_TOLERANCE 1.2
+#define N_GC 8876
+#define N_KVEC_PAIRS 625957
+#define Y_MAX 0.999999999992621
+#define Y_MIN 0.983276956198341
+#define TOL 0.41224489795918373
+#define P1 37.244897959183675
+#define P2 85.51020408163265 
+#define EPSILON_SEQ_ERROR 0.005346938775510204 
+#define EPSILON_EST 0.001 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define STAR_MIN_PIXEL 3
+#define STAR_MAX_PIXEL 150
+#define MAX_STARS 100
+#define SKIP_PIXELS 2
+#define LENGTH 1821
+#define BREADTH 1821
+#define PIXEL_WIDTH 0.00000155
+#define NUM_MAX_STARS 13
+//SM constants
+#define FOCAL_LENGTH 0.0175
+#define EPSILON 2.22e-15
+#define DELTA 4.9417133613238384e-05
+#define ANG_DIST_TOLERANCE 1.2
+#define N_GC 8876
+#define N_KVEC_PAIRS 625957
+#define Y_MAX 0.999999999992621
+#define Y_MIN 0.983276956198341
+#define TOL 0.41224489795918373
+#define P1 37.244897959183675
+#define P2 85.51020408163265 
+#define EPSILON_SEQ_ERROR 0.005346938775510204 
+#define EPSILON_EST 0.001 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define STAR_MIN_PIXEL 3
+#define STAR_MAX_PIXEL 150
+#define MAX_STARS 100
+#define SKIP_PIXELS 2
+#define LENGTH 1821
+#define BREADTH 1821
+#define PIXEL_WIDTH 0.00000155
+#define NUM_MAX_STARS 13
+//SM constants
+#define FOCAL_LENGTH 0.0175
+#define EPSILON 2.22e-15
+#define DELTA 4.9417133613238384e-05
+#define ANG_DIST_TOLERANCE 1.2
+#define N_GC 8876
+#define N_KVEC_PAIRS 625957
+#define Y_MAX 0.999999999992621
+#define Y_MIN 0.983276956198341
+#define TOL 0.41224489795918373
+#define P1 37.244897959183675
+#define P2 85.51020408163265 
+#define EPSILON_SEQ_ERROR 0.005346938775510204 
+#define EPSILON_EST 0.001 
---
+++ help.py
@@ -0,0 +1,2 @@
+for i in range(10):
+    print(f'python3 preprocess_image.py -f N/image_{i}.txt -m rga -i {i} -l 1821 -b 1821 -o image_{i}.h')
